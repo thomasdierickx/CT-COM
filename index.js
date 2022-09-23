c188cf0ed54994a9e676cb63c7ede3cc1e474672
Thomas Dierickx <thomas.dierickx3@student.howest.be>
2022-09-23 10:58:28 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const isDevelopment = (process.env.NODE_ENV === 'development');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('./localhost.key'),
+  cert: fs.readFileSync('./localhost.crt')
+};
+const server = require('https').Server(options, app); // httpS instead of http
+const port = process.env.PORT || 443;
+
+app.use(express.static('public'));
+
+server.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
+});
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const clients = {};
+io.on('connection', socket => {
+  clients[socket.id] = { id: socket.id };
+  socket.on(`message`, message => {
+    console.log(`Received message: ${message}`);
+    io.emit('message', message);
+  });
+
+  socket.on('color', color => {
+    console.log(`Received color: ${color}`);
+    io.emit('color', color);
+  })
+
+  socket.on('disconnect', () => {
+    delete clients[socket.id];
+    io.emit('clients', clients);
+  });
+
+  socket.on('signal', (peerId, signal) => {
+    console.log(`Received signal from ${socket.id} to ${peerId}`);
+    io.to(peerId).emit('signal', peerId, signal, socket.id);
+  });
+
+  io.emit('clients', clients);
+
+});
